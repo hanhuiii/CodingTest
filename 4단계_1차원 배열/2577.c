4a7d16d2d54c28f560a3f3dfdd64a2f427277bcd
hanhuiii <hangml16@ajou.ac.kr>
2021-12-29 13:50:25 +0900

    숫자의 개수_211229

--- 4단계_1차원 배열/2577.c
+++ 4단계_1차원 배열/2577.c
@@ -3,19 +3,20 @@
 
 int main() {
 	int a, b, c;
-	scanf("%d %d %d", &a, &b, &c);
 	int arr[10] = { 0, };
+
+	scanf("%d %d %d", &a, &b, &c);
 	int n = a * b * c;
-	//if (a, b, c >= 100 && a, b, c < 1000) {
-		while (n > 0) {
-			int num = n % 10;
-			arr[num]++;
-			n=n / 10;
-			//printf("%d\n", arr[num]);
-		}
-	//}
+
+	while (n > 0) {
+		int num = n % 10;
+		arr[num]++;
+		n=n / 10;
+
+	}
 	for (int i = 0; i < 10; i++) {
 		printf("%d\n", arr[i]);
 	}
+
 	return 0;
 }
